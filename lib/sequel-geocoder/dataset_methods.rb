1427d160c6ec4e1a4e9cd1319d3e336cffdc19d4
m.b <mbajur@users.noreply.github.com>
2017-06-26 00:59:57 +0200

    Update dataset_methods.rb

--- lib/sequel-geocoder/dataset_methods.rb
+++ lib/sequel-geocoder/dataset_methods.rb
@@ -35,9 +35,9 @@ module Sequel::Plugins::Geocoder
       sw_lat, sw_lng, ne_lat, ne_lng = bounds.flatten if bounds
       if sw_lat && sw_lng && ne_lat && ne_lng
         where(
-          Geocoder::Sql.within_bounding_box(sw_lat, sw_lng, ne_lat, ne_lng),
-          geocoder_options[:latitude],
-          geocoder_options[:longitude]
+          Geocoder::Sql.within_bounding_box(
+            sw_lat, sw_lng, ne_lat, ne_lng, geocoder_options[:latitude], geocoder_options[:longitude]
+          )
         )
       else
         geocoder_select_ds(nil, nil, nil).where(false)
