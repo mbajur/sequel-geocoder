401b0420c4a537d808e433841041bcf5bb3b1f1a
m.b <mbajur@users.noreply.github.com>
2017-06-26 01:21:23 +0200

    Update sequel-geocoder.rb

--- lib/sequel-geocoder.rb
+++ lib/sequel-geocoder.rb
@@ -3,6 +3,12 @@ require 'sequel'
 
 module Sequel::Plugins
   module Geocoder
+    def self.configure(model, opts={})
+      model.instance_eval do
+        @geocoder_options[:latitude] = opts[:latitude] || 'latitude'
+        @geocoder_options[:longitude] = opts[:longitude] || 'longitude'
+      end
+    end
   end
 end
 
